58ed6c98ce87a191d220a1b713f49160c2be629d
Cuong Vu <cuongvu0103@gmail.com>
2015-05-19 15:10:42 +0700

    Replace before(:each) with let in spec

--- spec/forms/registration_form_spec.rb
+++ spec/forms/registration_form_spec.rb
@@ -1,36 +1,37 @@
 require 'rails_helper'
 
 describe RegistrationForm do
+  let(:form) { described_class.new({email: 'cuongvu0103@gmail.com', name: 'Cuong Vu', terms_of_service: '1'}) }
+  let(:invalid_form) { described_class.new({email: ''}) }
+
   describe 'it does some validations' do
     it { should validate_acceptance_of :terms_of_service }
     it { should validate_presence_of :email }
     it { should validate_presence_of :name }
 
     context 'valid fields' do
-      before(:each) do
-        @form = described_class.new({email: 'cuongvu0103@gmail.com', name: 'Cuong Vu', terms_of_service: '1'})
-      end
-
       it 'validates form successfully' do
-        expect(@form).to be_valid
-      end
-
-      it 'returns true' do
-        expect(@form.save).to eql true
+        expect(form).to be_valid
       end
     end
 
     context 'invalid fields' do
-      before(:each) do
-        @form = described_class.new({})
-      end
-
       it 'fails to validate the form' do
-        expect(@form).to be_invalid
+        expect(invalid_form).to be_invalid
       end
+    end
+  end
 
+  describe 'it performs data' do
+    context 'valid fields' do
+      it 'returns true' do
+        expect(form.save).to eql true
+      end
+    end
+
+    context 'invalid fields' do
       it 'returns false' do
-        expect(@form.save).to eql false
+        expect(invalid_form.save).to eql false
       end
     end
   end
